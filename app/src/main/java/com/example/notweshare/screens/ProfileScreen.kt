ad3bac6e1429d61eb44a47ab39985d4284dd33fd
schwartz21 <pschw21@student.sdu.dk>
2023-11-16 19:03:15 +0100

    Added ProfileScreen

--- app/src/main/java/com/example/notweshare/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/notweshare/screens/ProfileScreen.kt
@@ -1,19 +1,146 @@
 package com.example.notweshare.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.BlendMode
-import androidx.navigation.NavController
-import com.example.exampleapplication.routes.Screen
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.notweshare.R
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
-fun ProfileScreen(navigation: NavController) {
-    Column {
-        Text(text = "ProfileScreen")
-        Button(onClick = {navigation.navigate(Screen.HomeScreen.route)}) {
-            Text(text = "Navigation to the 'HomeScreen' screen")
+fun ProfileScreen() {
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setStatusBarColor(MaterialTheme.colorScheme.primary)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        ProfileHeader()
+        Spacer(modifier = Modifier.height(16.dp))
+        ProfileDetails()
+    }
+}
+
+@Composable
+fun ProfileHeader() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = null,
+            modifier = Modifier
+                .size(80.dp)
+                .clip(CircleShape)
+                .background(MaterialTheme.colorScheme.primary)
+        )
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Column {
+            Text(
+                text = "John Doe",
+                style = MaterialTheme.typography.headlineSmall,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "Android Developer",
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
+}
+
+@Composable
+fun ProfileDetails() {
+    LazyColumn {
+        item {
+            ProfileItem(
+                icon = Icons.Default.Person,
+                title = "About Me",
+                description = "Passionate about Android development."
+            )
+        }
+        item {
+            ProfileItem(
+                icon = Icons.Default.Email,
+                title = "Email",
+                description = "john.doe@example.com"
+            )
+        }
+        item {
+            ProfileItem(
+                icon = Icons.Default.Phone,
+                title = "Phone",
+                description = "+1 234 567 890"
+            )
+        }
+        item {
+            ProfileItem(
+                icon = Icons.Default.Place,
+                title = "Location",
+                description = "City, Country"
+            )
+        }
+    }
+}
+
+@Composable
+fun ProfileItem(icon: ImageVector, title: String, description: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.primary
+        )
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Column {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.primary
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = description, style = MaterialTheme.typography.bodyMedium)
         }
     }
 }
