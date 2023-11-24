dbe73a73ec2cd8a7ed202b00f0d353e25c60e41a
schwartz21 <pschw21@student.sdu.dk>
2023-11-24 20:19:38 +0100

    Removed imports

--- app/src/main/java/com/example/notweshare/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/notweshare/screens/ProfileScreen.kt
@@ -25,7 +25,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
