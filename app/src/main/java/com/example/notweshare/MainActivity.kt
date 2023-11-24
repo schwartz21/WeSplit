675b36016df878aee199aeb2dfffdd549533275b
schwartz21 <pschw21@student.sdu.dk>
2023-11-24 13:11:20 +0100

    Added TabBar to the application

--- app/src/main/java/com/example/notweshare/MainActivity.kt
+++ app/src/main/java/com/example/notweshare/MainActivity.kt
@@ -1,19 +1,36 @@
 package com.example.notweshare
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.exampleapplication.routes.Screen
+import com.example.notweshare.models.TabItem
 import com.example.notweshare.screens.HomeScreen
 import com.example.notweshare.screens.NewGroupScreen
 import com.example.notweshare.screens.ProfileScreen
@@ -33,19 +50,54 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun Navigation(name: String, modifier: Modifier = Modifier) {
-
-    val navigation = rememberNavController();
-
-    Column {
-        NavHost(
-            navController = navigation,
-            startDestination = Screen.ProfileScreen.route
-        ){
-            composable(Screen.HomeScreen.route) { HomeScreen(navigation = navigation) }
-            composable(Screen.NewGroupScreen.route) { NewGroupScreen(navigation = navigation)}
-            composable(Screen.ProfileScreen.route) { ProfileScreen(navigation = navigation)}
+    val navController = rememberNavController()
+    val tabs = listOf(
+        TabItem("Home", R.drawable.home),
+        TabItem("Groups", R.drawable.group),
+        TabItem("Profile", R.drawable.profile)
+    )
+    var selectedTabIndex by remember { mutableStateOf(0) }
+    Scaffold(
+        bottomBar = {
+            BottomAppBar(
+                contentColor = Color.White,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                tabs.forEachIndexed { index, tab ->
+                    IconButton(
+                        onClick = {
+                            selectedTabIndex = index
+                            navController.navigate(index.toString())
+                        },
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Bottom
+                        ) {
+                            Image(
+                                painter = painterResource(tab.icon),
+                                contentDescription = null,
+                                modifier = Modifier.size(24.dp)
+                            )
+                            Text(
+                                text = tab.title,
+                                style = MaterialTheme.typography.bodySmall,
+                                color = if (selectedTabIndex == index) MaterialTheme.colorScheme.onBackground else MaterialTheme.colorScheme.onBackground,
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    ) {
+        NavHost(navController, startDestination = "0") {
+            composable("0") { HomeScreen(navigation = navController) }
+            composable("1") { NewGroupScreen(navigation = navController) }
+            composable("2") { ProfileScreen(navigation = navController) }
         }
     }
 }
@@ -57,12 +109,3 @@ fun GreetingPreview() {
         Navigation("for the Android")
     }
 }
-
-
-//private fun fetchUsers() {
-//    FetchUsers.findUsers(FirestoreQueries.UserQueries.userWithPhoneNumber("20838848")) { userArray ->
-//        if (userArray.isNotEmpty()) {
-//            print(userArray.first().name)
-//        }
-//    }
-//}
