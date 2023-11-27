abbdc6201379cdde4ddb6bc1f40fa166f21513f2
TobiasBak <tobib21@student.sdu.dk>
2023-11-27 16:45:57 +0100

    Live update userViewModel

--- app/src/main/java/com/example/notweshare/viewmodels/UserViewModel.kt
+++ app/src/main/java/com/example/notweshare/viewmodels/UserViewModel.kt
@@ -1,17 +1,22 @@
 package com.example.exampleapplication.viewmodels
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.notweshare.backend.FirestoreQueries
+import com.example.notweshare.models.Group
 import com.example.notweshare.models.User
-import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.QuerySnapshot
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
 
 class UserViewModel(): ViewModel() {
+    var listener: ListenerRegistration? = null
     val users = mutableStateListOf<User>()
     var isLoading = mutableStateOf(false)
 
@@ -58,30 +63,35 @@ class UserViewModel(): ViewModel() {
         }
     }
 
-    private suspend fun fetchUsers(queryCondition: Query, callback: (MutableList<User>) -> Unit) {
-        val userArray: MutableList<User> = mutableListOf<User>()
+    private fun fetchUsers(queryCondition: Query, callback: (MutableList<User>) -> Unit) {
+        var userArray: MutableList<User> = mutableListOf<User>()
 
-        val task = queryCondition.get()
-        task.addOnCompleteListener {
-            if (it.isSuccessful) {
-                val documents = it.result
-                if (documents != null) {
-                    for (doc in documents) {
-                        val user = doc.toObject(User::class.java)
-                        user.documentID = doc.id
-                        userArray.add(user)
-                    }
-                    callback(userArray)
+        listener = queryCondition.addSnapshotListener(object : EventListener<QuerySnapshot> {
+            override fun onEvent(
+                value: QuerySnapshot?,
+                error: FirebaseFirestoreException?
+            ) {
+                userArray = mutableListOf()
+
+                if (error != null) {
+                    Log.e("Firestore error", error.message.toString())
+                    callback(mutableListOf())
                 }
+
+                value?.let { actualValue ->
+                    for (document in actualValue.documents) {
+                        Log.d("Success", document.toString())
+
+                        document.toObject(User::class.java)?.let { foundObject ->
+                            foundObject.documentID = document.id
+                            userArray.add(foundObject)
+                        }
+                    }
+                }
+
+                callback(userArray)
             }
-        }
-        task.await()
+        })
     }
 }
 
-private fun <TResult> Task<TResult>.addOnCompleteListener(
-    userViewModel: UserViewModel,
-    any: Any
-): Any {
-    return this
-}
