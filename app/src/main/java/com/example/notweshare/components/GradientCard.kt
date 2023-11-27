1f217dce9b62528e60370ff7523768d737b1f37e
Thor Malmby Jørgin <tjoer21@student.sdu.dk>
2023-11-27 16:20:55 +0100

    Gradient card with input field
    what the hell man

--- app/src/main/java/com/example/notweshare/components/GradientCard.kt
+++ app/src/main/java/com/example/notweshare/components/GradientCard.kt
@@ -9,15 +9,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -60,13 +66,14 @@ fun GradientCard(
                     ),
                 contentAlignment = Alignment.Center
             ) {
-                Text(
-                    text = text,
-                    modifier = Modifier.padding(smallPadding),
-                    color = MaterialTheme.colorScheme.onPrimary,
-                    style = MaterialTheme.typography.titleLarge,
-                    textAlign = TextAlign.Center,
-                )
+//                Text(
+//                    text = text,
+//                    modifier = Modifier.padding(smallPadding),
+//                    color = MaterialTheme.colorScheme.onPrimary,
+//                    style = MaterialTheme.typography.titleLarge,
+//                    textAlign = TextAlign.Center,
+//                )
+                HeaderDisplay(string = text)
             }
             Row(
                 horizontalArrangement = Arrangement.SpaceEvenly,
@@ -79,4 +86,30 @@ fun GradientCard(
         }
     }
 
+}
+
+@Composable
+fun HeaderDisplay(string: String){
+    val displayText = remember{
+        mutableStateOf(string)
+    }
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth(),
+        colors = OutlinedTextFieldDefaults.colors(
+//            focusedContainerColor = MaterialTheme.colorScheme.background,
+//            unfocusedContainerColor = MaterialTheme.colorScheme.background,
+//            disabledContainerColor = MaterialTheme.colorScheme.onBackground,
+//            cursorColor = MaterialTheme.colorScheme.onBackground,
+//            focusedBorderColor = MaterialTheme.colorScheme.primary,
+//            focusedLabelColor = MaterialTheme.colorScheme.onBackground,
+        ),
+        textStyle = MaterialTheme.typography.titleLarge.copy(textAlign = TextAlign.Center, color = MaterialTheme.colorScheme.onPrimary),
+        shape = textFieldShape,
+        keyboardOptions = KeyboardOptions.Default,
+        value = displayText.value,
+        onValueChange = {
+            displayText.value = it
+        }
+    )
 }
