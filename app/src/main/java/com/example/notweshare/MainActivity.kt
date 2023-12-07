7f46c7a2c1f8adc29ec3d350f7054a0323520109
Rasmus Christiansen <rachristi@outlook.dk>
2023-12-07 19:17:03 +0100

    Merge

--- app/src/main/java/com/example/notweshare/MainActivity.kt
+++ app/src/main/java/com/example/notweshare/MainActivity.kt
@@ -56,7 +56,6 @@ import com.example.notweshare.screens.PermissionDialog
 import com.example.notweshare.screens.ProfileScreen
 import com.example.notweshare.screens.LoginScreen
 import com.example.notweshare.screens.RegisterScreen
-import com.example.notweshare.ui.theme.NotWeShareTheme
 import com.google.firebase.FirebaseApp
 import com.google.firebase.messaging.FirebaseMessaging
 
@@ -76,201 +75,199 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Navigation()
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Navigation("for the Android 2")
-                    Button(onClick= {startNotificationService()}){
-                      Text(text = "CLICK ME")
+                    Button(onClick = { startNotificationService() }) {
+                        Text(text = "CLICK ME")
                     }
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                         PermissionDialog()
                     }
                 }
             }
         }
-    }
 
-    private fun getFireBaseToken() {
-        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                //save this token to the user and store in DB for late use.
-                //dont know if we should check if the token is the same as previous every time the app launches
-                val token = task.result
-                Log.d("NotificationService", token)
-                println(token)
-                // Send this token to your server to identify and target specific devices
-            } else {
-                Log.e("FCM Token", "Failed to get token: ${task.exception}")
+        private fun getFireBaseToken() {
+            FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    //save this token to the user and store in DB for late use.
+                    //dont know if we should check if the token is the same as previous every time the app launches
+                    val token = task.result
+                    Log.d("NotificationService", token)
+                    println(token)
+                    // Send this token to your server to identify and target specific devices
+                } else {
+                    Log.e("FCM Token", "Failed to get token: ${task.exception}")
+                }
             }
         }
-    }
-    private fun startNotificationService() {
-        // Create an intent to start the com.example.notweshare.notification.NotificationJobIntentService
-        val intent = Intent(this, NotificationJobIntentService::class.java)
-        intent.putExtra("title", "Notification Title")
-        intent.putExtra("body", "Notification Body")
 
-        // Enqueue the work to be done by the service
-        NotificationJobIntentService.enqueueWork(this, intent)
-    }
-}
+        private fun startNotificationService() {
+            // Create an intent to start the com.example.notweshare.notification.NotificationJobIntentService
+            val intent = Intent(this, NotificationJobIntentService::class.java)
+            intent.putExtra("title", "Notification Title")
+            intent.putExtra("body", "Notification Body")
 
-@Composable
-fun Navigation() {
-    val navController = rememberNavController()
-    var selectedTabIndex by remember { mutableStateOf(0) }
-    val tabs = listOf(
-        TabItem("Groups", Screen.HomeScreen.route, R.drawable.group),
-        TabItem("Create Group", Screen.NewGroupScreen.route, R.drawable.square_add),
-        TabItem("Profile", Screen.ProfileScreen.route, R.drawable.profile)
-    )
-
-
-    val tabBarHeight = with(LocalDensity.current) {
-        65.sp.toDp()
+            // Enqueue the work to be done by the service
+            NotificationJobIntentService.enqueueWork(this, intent)
+        }
     }
 
-    val contentHeight = LocalConfiguration.current.screenHeightDp.dp - tabBarHeight
+    @Composable
+    fun Navigation() {
+        val navController = rememberNavController()
+        var selectedTabIndex by remember { mutableStateOf(0) }
+        val tabs = listOf(
+            TabItem("Groups", Screen.HomeScreen.route, R.drawable.group),
+            TabItem("Create Group", Screen.NewGroupScreen.route, R.drawable.square_add),
+            TabItem("Profile", Screen.ProfileScreen.route, R.drawable.profile)
+        )
 
-    // If We wish to change the highlighted tab, we should probably do it by modifiying an external variable
 
-    Scaffold(
-        bottomBar = {
-            if (userViewModel.activeUser.value.documentID.isNotEmpty()) {
-                BottomAppBar(
-                    containerColor = MaterialTheme.colorScheme.onSecondaryContainer,
-                    contentColor = Color.White,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(tabBarHeight)
-                ) {
-                    tabs.forEachIndexed { index, tab ->
-                        val tint =
-                            ColorFilter.tint(if (selectedTabIndex == index) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onBackground)
-                        IconButton(
-                            onClick = {
-                                // This is the variable that must be changed to change the selected tab
-                                selectedTabIndex = index
-                                navController.navigate(tab.route)
-                            },
-                            modifier = Modifier.weight(1f)
-                        ) {
-                            Column(
-                                horizontalAlignment = Alignment.CenterHorizontally,
-                                verticalArrangement = Arrangement.Bottom
+        val tabBarHeight = with(LocalDensity.current) {
+            65.sp.toDp()
+        }
+
+        val contentHeight = LocalConfiguration.current.screenHeightDp.dp - tabBarHeight
+
+        // If We wish to change the highlighted tab, we should probably do it by modifiying an external variable
+
+        Scaffold(
+            bottomBar = {
+                if (userViewModel.activeUser.value.documentID.isNotEmpty()) {
+                    BottomAppBar(
+                        containerColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                        contentColor = Color.White,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(tabBarHeight)
+                    ) {
+                        tabs.forEachIndexed { index, tab ->
+                            val tint =
+                                ColorFilter.tint(if (selectedTabIndex == index) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onBackground)
+                            IconButton(
+                                onClick = {
+                                    // This is the variable that must be changed to change the selected tab
+                                    selectedTabIndex = index
+                                    navController.navigate(tab.route)
+                                },
+                                modifier = Modifier.weight(1f)
                             ) {
-                                Image(
-                                    colorFilter = tint,
-                                    painter = painterResource(tab.icon),
-                                    contentDescription = null,
-                                    modifier = Modifier.size(24.dp)
-                                )
-                                Text(
-                                    text = tab.title,
-                                    style = MaterialTheme.typography.bodySmall,
-                                    color = if (selectedTabIndex == index) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onBackground,
-                                )
+                                Column(
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.Bottom
+                                ) {
+                                    Image(
+                                        colorFilter = tint,
+                                        painter = painterResource(tab.icon),
+                                        contentDescription = null,
+                                        modifier = Modifier.size(24.dp)
+                                    )
+                                    Text(
+                                        text = tab.title,
+                                        style = MaterialTheme.typography.bodySmall,
+                                        color = if (selectedTabIndex == index) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onBackground,
+                                    )
+                                }
                             }
                         }
                     }
                 }
             }
-        }
-    ) {
-        NavHost(
-            navController,
-            startDestination = Screen.LoginScreen.route,
-            modifier = Modifier.height(
-                contentHeight
-            )
         ) {
-            with(navController) {
-                composable(Screen.LoginScreen.route) {
-                    LoginScreen(
-                        navigateToRegister = {
-                            navigate(
-                                Screen.RegisterScreen.route
-                            )
-                        },
-                        navigateToHomeScreen = {
-                            // Find groups with the user as a member
-                            groupViewModel.findGroupsWithMember(userViewModel.activeUser.value.documentID)
-                            navigate(
-                                Screen.HomeScreen.route
-                            )
-                        },
-                    )
+            NavHost(
+                navController,
+                startDestination = Screen.LoginScreen.route,
+                modifier = Modifier.height(
+                    contentHeight
+                )
+            ) {
+                with(navController) {
+                    composable(Screen.LoginScreen.route) {
+                        LoginScreen(
+                            navigateToRegister = {
+                                navigate(
+                                    Screen.RegisterScreen.route
+                                )
+                            },
+                            navigateToHomeScreen = {
+                                // Find groups with the user as a member
+                                groupViewModel.findGroupsWithMember(userViewModel.activeUser.value.documentID)
+                                navigate(
+                                    Screen.HomeScreen.route
+                                )
+                            },
+                        )
+                    }
+                    composable(Screen.RegisterScreen.route) {
+                        RegisterScreen(
+                            navigateToHomeScreen = {
+                                // Find groups with the user as a member
+                                groupViewModel.findGroupsWithMember(userViewModel.activeUser.value.documentID)
+                                navigate(
+                                    Screen.HomeScreen.route
+                                )
+                            },
+                            navigateToLogin = {
+                                navigate(
+                                    Screen.LoginScreen.route
+                                )
+                            },
+                        )
+                    }
+                    composable(Screen.HomeScreen.route) {
+                        HomeScreen(
+                            navigateToGroupDetails = {
+                                navigate(
+                                    Screen.GroupDetailsScreen.route
+                                )
+                            },
+                        )
+                    }
+                    composable(Screen.NewGroupScreen.route) {
+                        NewGroupScreen(
+                            navigateToGroups = {
+                                navigate(
+                                    Screen.HomeScreen.route
+                                )
+                            },
+                        )
+                    }
+                    composable(Screen.ProfileScreen.route) { ProfileScreen() }
+                    composable(Screen.GroupDetailsScreen.route) {
+                        GroupDetailsScreen(
+                            navigateToNewExpense = {
+                                navigate(
+                                    Screen.NewExpenseScreen.route
+                                )
+                            },
+                            context = context,
+                        )
+                    }
+                    composable(Screen.NewExpenseScreen.route) {
+                        NewExpenseScreen(
+                            navigateUp = {
+                                navigateUp()
+                            },
+                        )
+                    }
                 }
-                composable(Screen.RegisterScreen.route) {
-                    RegisterScreen(
-                        navigateToHomeScreen = {
-                            // Find groups with the user as a member
-                            groupViewModel.findGroupsWithMember(userViewModel.activeUser.value.documentID)
-                            navigate(
-                                Screen.HomeScreen.route
-                            )
-                        },
-                        navigateToLogin = {
-                            navigate(
-                                Screen.LoginScreen.route
-                            )
-                        },
-                    )
-                }
-                composable(Screen.HomeScreen.route) {
-                    HomeScreen(
-                        navigateToGroupDetails = {
-                            navigate(
-                                Screen.GroupDetailsScreen.route
-                            )
-                        },
-                    )
-                }
-                composable(Screen.NewGroupScreen.route) {
-                    NewGroupScreen(
-                        navigateToGroups = {
-                            navigate(
-                                Screen.HomeScreen.route
-                            )
-                        },
-                    )
-                }
-                composable(Screen.ProfileScreen.route) { ProfileScreen() }
-                composable(Screen.GroupDetailsScreen.route) {
-                    GroupDetailsScreen(
-                        navigateToNewExpense = {
-                            navigate(
-                                Screen.NewExpenseScreen.route
-                            )
-                        },
-                        context = context,
-                    )
-                }
-                composable(Screen.NewExpenseScreen.route) {
-                    NewExpenseScreen(
-                        navigateUp = {
-                            navigateUp()
-                        },
-                    )
-                }
+
             }
 
+            // THIS FOLLOWING LINE MUST BE THERE FOR THE LINTER TO WORK
+            val something = it
         }
+    }
 
-        // THIS FOLLOWING LINE MUST BE THERE FOR THE LINTER TO WORK
-        val something = it
+    sealed class Screen(val route: String) {
+        object HomeScreen : Screen("HomeScreen")
+        object NewGroupScreen : Screen("NewScreen")
+        object ProfileScreen : Screen("ProfileScreen")
+        object GroupDetailsScreen : Screen("GroupDetailsScreen")
+        object NewExpenseScreen : Screen("NewExpenseScreen")
+        object LoginScreen : Screen("LoginScreen")
+        object RegisterScreen : Screen("RegisterScreen")
     }
 }
-
-sealed class Screen(val route: String) {
-    object HomeScreen : Screen("HomeScreen")
-    object NewGroupScreen : Screen("NewScreen")
-    object ProfileScreen : Screen("ProfileScreen")
-    object GroupDetailsScreen : Screen("GroupDetailsScreen")
-    object NewExpenseScreen : Screen("NewExpenseScreen")
-    object LoginScreen : Screen("LoginScreen")
-    object RegisterScreen : Screen("RegisterScreen")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
